6c22e38f40d02f6662ef8560d8f19d27a6321c6e
Anfmx <vlad.anfimov.1997@gmail.com>
2025-02-23 14:45:01 +0300

    db logic added

--- db/db.go
+++ db/db.go
@@ -2,45 +2,35 @@ package db
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"time"
 
+	"github.com/charmbracelet/bubbles/table"
 	"github.com/joho/godotenv"
+	"github.com/username/tui-db-manager/tables"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var counter int
 
-type User struct {
-	ID             uint `gorm:"primarykey"`
-	Name           string
-	AccountCreated time.Time
-}
-
-func ConnectDB() (*gorm.DB, error) {
+func ConnectDB(port, username, password string) (*gorm.DB, error) {
 	err := godotenv.Load("./.env")
 	if err != nil {
-		log.Fatal("env file loading error")
+		fmt.Println("Loading env file error:", err)
 	}
 
-	port := os.Getenv("PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
-
-	dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=testdb port=%s sslmode=disable", dbUser, dbPass, port)
+	dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=testdb port=%s sslmode=disable", username, password, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&tables.User{})
 	return db, nil
 }
 
 func CreateTable(db *gorm.DB) error {
-	if !db.Migrator().HasTable(&User{}) {
-		if err := db.Migrator().CreateTable(&User{}); err != nil {
+	if !db.Migrator().HasTable(&tables.User{}) {
+		if err := db.Migrator().CreateTable(&tables.User{}); err != nil {
 			return err
 		}
 	}
@@ -48,7 +38,7 @@ func CreateTable(db *gorm.DB) error {
 }
 
 func DropTable(db *gorm.DB) error {
-	if err := db.Migrator().DropTable(&User{}); err != nil {
+	if err := db.Migrator().DropTable(&tables.User{}); err != nil {
 		return err
 	}
 	return nil
@@ -57,8 +47,8 @@ func DropTable(db *gorm.DB) error {
 func CreatRow(db *gorm.DB) {
 	counter++
 
-	userExample := User{Name: fmt.Sprintf("Test %d", counter)}
-	db.Select(&User{}).Create(&userExample)
+	userExample := tables.User{Name: fmt.Sprintf("Test %d", counter), AccountCreated: time.Now()}
+	db.Select(&tables.User{}).Create(&userExample)
 
 	newUser := userExample
 	newUser.ID = 0
@@ -67,5 +57,35 @@ func CreatRow(db *gorm.DB) {
 }
 
 func DeleteLastRow(db *gorm.DB) {
-	db.Last(&User{}).Delete(&User{})
+	db.Last(&tables.User{}).Delete(&tables.User{})
+}
+
+func GetColumns(db *gorm.DB) ([]table.Column, error) {
+	var columns []table.Column
+
+	rows, err := db.Raw(`
+	SELECT column_name
+	FROM information_schema.columns
+	WHERE table_name = 'users' AND table_schema = 'public'`).Rows()
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var columnName string
+		if err := rows.Scan(&columnName); err != nil {
+			return nil, err
+		}
+		columns = append(columns, table.Column{
+			Title: columnName,
+			Width: 15,
+		})
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return columns, nil
 }
